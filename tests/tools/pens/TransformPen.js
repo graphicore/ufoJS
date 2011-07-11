2e017dd453287ebacf28ab5398456bfa5492c060
Lasse Fister <commander@graphicore.de>
2011-07-12 00:16:45 +0200

    adding tests for the transform pen and for that the AbstractTestPen that enhances AbstractPen, now the former TestPen is called BaseTestPen and that one still enhances BasePen. Both reside in the TestPens module. //for the record: using the BaseTestPen was more like testing BasePen but to do so TransformPen must work. That's a cyclic reference in a way and avoidable here

--- tests/tools/pens/TransformPen.js
+++ tests/tools/pens/TransformPen.js
@@ -1,37 +1,77 @@
 define(
-    ['graphicore/errors', 'graphicore/tools/pens/TestPen', 'graphicore/tools/pens/TransformPen'],
-    function(errors, TestPen, TransformPen)
+    [
+        'graphicore/errors',
+        'graphicore/tools/pens/TestPens',
+        'graphicore/tools/pens/TransformPen',
+        'graphicore/tools/misc/transform'
+    ],
+    function(errors, TestPens, TransformPen, transform)
 {
+    /*shortcuts*/
+    var TestPen = TestPens.AbstractTestPen,
+        Transform = transform.Transform;
+    
     doh.register("graphicore.pens.TransformPen", [
-    function Test_WithTestPen(){
-        /*just to get started*/
+    function Test_TransformPen(){
+        /*
+         * just to get started
+         * all the transformation foo is tested with the tools.misc.transform
+         * module tests, so this is just to proove that the Transform pen
+         * behaves like it should
+         * */
         var testPen = new TestPen(null),
-            pen = new TransformPen(testPen, [2, 0, 0.5, 2, -10, 0]),
+            testTransformation = [2, 0, 0.5, 2, -10, 0],
+            pen = new TransformPen(testPen, testTransformation),
             expecting, result;
         
-        expecting = [['_moveTo', [-10, 0]]]
+        expecting = [ ['moveTo', [-10, 0]] ]
         pen.moveTo([0, 0]);
         doh.assertEqual(expecting, testPen.flush());
         
-        expecting = [['_lineTo', [40.0, 200]]];
+        expecting = [ ['lineTo', [40.0, 200]] ];
         pen.lineTo([0, 100]);
         doh.assertEqual(expecting, testPen.flush());
         
         expecting = [
-            ['_curveToOne', [127.5, 150], [131.25, 125.0], [125.0, 100.0]],
-            ['_curveToOne', [118.75, 75.0], [102.5, 50], [-10.0, 0]]
+            ['curveTo', [127.5, 150], [135.0, 100], [102.5, 50], [-10.0, 0]]
         ];
         pen.curveTo([50, 75], [60, 50], [50, 25], [0, 0]);
         doh.assertEqual(expecting, testPen.flush());
         
-        //moar!
-        //try pen.qCurveTo([50, 75], [60, 50], [50, 25], [0, 0]);
-        //fails in python => investiagte
         
-        
-        expecting = [['_closePath']];
-        pen.closePath();
+        pen.qCurveTo([50, 75], [60, 50], [50, 25], [0, 0]);
+        expecting = [
+            ['qCurveTo', [127.5, 150], [135.0, 100], [102.5, 50], [-10.0, 0]]
+        ];
+        //seems to fail in python => investigate
         doh.assertEqual(expecting, testPen.flush());
+        
+        pen.qCurveTo([50, 75], [60, 50], null);
+        expecting = [
+            ['qCurveTo', [127.5, 150], [135.0, 100], null],
+        ];
+        doh.assertEqual(expecting, testPen.flush());
+        
+        pen.closePath();
+        expecting = [ ['closePath'] ];
+        doh.assertEqual(expecting, testPen.flush());
+        
+        // its not possible to test the returned transformation matrix
+        // within the assertEquals call, so we test that first.
+        // we don't really transform ther matrix with the identity
+        // transformation
+        pen.addComponent('myComponent', transform.Identity);
+        expecting = [
+            ['addComponent', 'myComponent'],
+        ];
+        var result = testPen.flush();
+        //result[2] is expected to be a misc.transform.Transform Object
+        doh.assertTrue(result[0][2] instanceof Transform);
+        //the Transform object has a cmp method to compare values against it
+        doh.assertTrue(result[0][2].cmp(testTransformation));
+        //now lets just see if the rest is good
+        delete(result[0][2]);
+        doh.assertEqual(expecting, result);
     }
     ]);
 });
