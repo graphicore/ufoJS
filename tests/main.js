26792597014559d1fdc541ac376d064e8bbfe6a7
Lasse Fister <commander@graphicore.de>
2011-08-31 22:03:35 +0200

    tests/main is now only a layer to require all tests

--- tests/main.js
+++ tests/main.js
@@ -1,45 +1,14 @@
-dojo.provide('graphicore.tests.main');
-/**
- * this is a little hackish. We use a doh test module to bootstrap
- * a test environment that suits us better, using the backdraft loader
- * and its configuration
- **/
-
-// delete the define implementation of dojo to enable loading of the
-// backdraft loader. require js will not load when a global var 'define'
-// exists. There might be no 'define' declared by dojo when you use the
-// built version of dojo or an old one. At least dojo-release-1.6.1-src
-// defines 'define'
-delete(define);
-
-//setup the backdraft loader
-var require = {
-    baseUrl: '/javascript',
-    packages:[
-        {
-            name: 'graphicore',
-        },
-        {
-            name: 'tests',
-            lib: '',
-            location:'graphicore/tests'
-        }
-    ]
-}
-
-//use dojo internals to load the backdraft loader syncronous
-dojo._loadUri('../../require.js');
-
-//now use backdraft to load our tests and execute them when ready
-require([
-        'tests/errors',
-        'tests/testmain',
-        'tests/tools/pens/AbstractPen',
-        'tests/tools/pens/main',
-        'tests/tools/misc/transform',
-        'tests/tools/pens/TransformPen',
-        'tests/tools/pens/BasePen',
-        'tests/tools/pens/AbstractPointPen',
-        'tests/tools/pens/BasePointToSegmentPen',
-        'tests/tools/pens/PointToSegmentPen'
-    ], function(){doh.run();});
+//this is just a layer to load all the tests. they will register themselves
+//with the global! doh object.
+define([
+    'tests/errors',
+    'tests/testmain',
+    'tests/tools/pens/AbstractPen',
+    'tests/tools/pens/main',
+    'tests/tools/misc/transform',
+    'tests/tools/pens/TransformPen',
+    'tests/tools/pens/BasePen',
+    'tests/tools/pens/AbstractPointPen',
+    'tests/tools/pens/BasePointToSegmentPen',
+    'tests/tools/pens/PointToSegmentPen'
+],'test Layer');
