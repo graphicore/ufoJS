06a663da7f52984e012c03935f0ef56abeb816e1
Lasse Fister <commander@graphicore.de>
2014-05-23 15:22:34 +0200

    simple testcase for the serializer

--- tests/xml/main.js
+++ tests/xml/main.js
@@ -6,6 +6,8 @@ define(
      * TODO: Testing the xml Module should be done far more, as this is a
      * point where the implementations from the varoius Environments differ
      * strongly.
+     * 
+     * FIXME: test all of the exposed API
      */
     doh.register("xml.main", [
         function Test_parseFromString() {
@@ -28,7 +30,7 @@ define(
          * 
          * So here is a test to see how well our workarounds do.
          */
-        function Test_parseFromString_BadInput(){
+        function Test_parseFromString_BadInput() {
             var parser = new xml.Parser();
             doh.assertError(
                 errors.Parser,
@@ -43,9 +45,26 @@ define(
                 ['<broken', 'text/xml'],
                 'Broken XML must raise ParserError'
             )
+        },
+        function Test_serializeToString() {
+            var parser = new xml.Parser()
+              , doc = parser.parseFromString(
+                    '<root><child attr="attribute content">Content!</child></root>'
+                  , 'text/xml')
+              , serializer = new xml.Serializer()
+              , doc2, roundtripped
+              , result
+              ;
+            
+            result = serializer.serializeToString(doc)
+            
+            doh.assertEqual(0, result.indexOf('<?xml'));
+            
+            doc2 = parser.parseFromString(result, 'text/xml')
+            roundtripped = serializer.serializeToString(doc2)
+            
+            doh.assertEqual(result, roundtripped);
         }
-        
-        
     ])
     
     
